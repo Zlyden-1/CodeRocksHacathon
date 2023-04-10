3b0af5debe976721bffd80299c2aca1ff8479675
Максим Авдошин <Zlyden-1@yandex.ru>
2023-04-10 14:52:52 +0400

    Added orders model

--- backend/servise_board/board/models.py
+++ backend/servise_board/board/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -33,6 +35,46 @@ class Contractor(User):
         permissions = ()
 
 
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=200, verbose_name='Статус')
 
+    def __str__(self):
+        return self.status
+
+    class Meta:
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказов'
+
+
+class Order(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    detail = models.TextField(verbose_name='Описане')
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Заказчик')
+    status = models.ForeignKey(to=OrderStatus, on_delete=models.PROTECT, verbose_name='Статус')
+
+    def delete(self, *args, **kwargs):
+        for photo in self.orderphoto_set.all():
+            photo.delete()
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class OrderPhoto(models.Model):
+    photo = models.ImageField(upload_to='orders/photos/', verbose_name='Фото')
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name='Заказчик')
+
+    def filename(self):
+        return os.path.basename(self.photo.name)
+
+    def delete(self, *args, **kwargs):
+        photo_path = self.photo.path
+        os.remove(photo_path)
+        super().delete(*args, **kwargs)
 
 
