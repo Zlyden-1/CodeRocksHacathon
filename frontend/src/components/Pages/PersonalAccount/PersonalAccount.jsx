da356ce1f8d100df75a617f835cadad0a1be712e
mlakkerman <nonamesyntax@mail.ru>
2023-04-13 14:39:32 +0400

    changes MainPage

--- frontend/src/components/Pages/PersonalAccount/PersonalAccount.jsx
+++ frontend/src/components/Pages/PersonalAccount/PersonalAccount.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import s from './PersonalAccount.module.scss'
-import { CategoriesCard } from "../../Cards/CategoriesCard/CategoriesCard";
-import axios from "axios";
-import { useState } from "react";
-import { useEffect } from 'react';
 import { AiFillStar } from 'react-icons/ai'
 import { SiGooglemaps } from 'react-icons/si'
 import { OrderCard } from "../../Cards/OrderCard/OrderCard";
